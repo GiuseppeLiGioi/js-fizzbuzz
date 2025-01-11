d714247f516a1d195280f903e97981ebaa0d0d57
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-01-11 14:53:39 +0100

    ADD: if and else if, fine esercizio

--- main.js
+++ main.js
@@ -19,14 +19,14 @@ TASK:
 
 
   for (let i = 0; i<=100; i++){
-    if(i % 3 == 0){
+    if(i % 3 == 0 && i % 5 == 0 ){
+        console.log("FizzBuzz")
+    }
+    else if (i % 3 == 0){
         console.log("Fizz")
     }
     else if (i % 5 == 0){
         console.log("Buzz")
-    }
-    else if (i % 3 == 0 && i % 5==0){
-        console.log("FizzBuzz")
     }
     else{
         console.log(`${i}`)
